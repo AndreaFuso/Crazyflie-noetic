83e659f34429cad66f7998a2bf2bcc03cc58024c
Andrea Fuso <andrea1.fuso@gmail.com>
2021-06-25 15:20:03 +0200

    Working on pids (just thrust)

--- crazy_common_py/src/crazy_common_py/constants.py
+++ crazy_common_py/src/crazy_common_py/constants.py
@@ -29,43 +29,65 @@ ACTUATOR_VELOCITY_CONTROLLER_KI = 1.0
 ACTUATOR_VELOCITY_CONTROLLER_KD = 0.0
 ACTUATOR_VELOCITY_CONTROLLER_I_CLAMP = 100.0
 
-PID_ROLL_RATE_KP = 0.52    #250
-PID_ROLL_RATE_KI = 0.52    #500
-PID_ROLL_RATE_KD = 0.025      #2.5
+PID_ROLL_RATE_KP = 0.25    #250 0.52
+PID_ROLL_RATE_KI = 0.5    #500 0.52
+PID_ROLL_RATE_KD = 3.0     #2.5 0.025
 PID_ROLL_RATE_INTEGRATION_LIMIT = 33.3      #33.3
 
-PID_PITCH_RATE_KP = 0.52   #250
-PID_PITCH_RATE_KI = 0.52  #500
-PID_PITCH_RATE_KD = 0.025     #2.5
-PID_PITCH_RATE_INTEGRATION_LIMIT = 33.3 #33.3
+PID_PITCH_RATE_KP = 0.25   #250 0.52
+PID_PITCH_RATE_KI = 0.5  #500 0.52
+PID_PITCH_RATE_KD = 3.0     #2.5 0.025
+PID_PITCH_RATE_INTEGRATION_LIMIT = 0.0 #33.3
 
-PID_YAW_RATE_KP = 0.52 #120
-PID_YAW_RATE_KI = 0.52  #16.7
-PID_YAW_RATE_KD = 0.025   #0.0
-PID_YAW_RATE_INTEGRATION_LIMIT = 166.7  #166.7
+PID_YAW_RATE_KP = 0.12 #120 0.52
+PID_YAW_RATE_KI = 0.167  #16.7 0.52
+PID_YAW_RATE_KD = 3.0   #0.0 0.025
+PID_YAW_RATE_INTEGRATION_LIMIT = 0.7  #166.7
 
-PID_ROLL_KP = 3.0   #6.0
-PID_ROLL_KI = 3.0   #3.0
-PID_ROLL_KD = 0.035   #0.0
-PID_ROLL_INTEGRATION_LIMIT = 20.0   #20.0
+PID_ROLL_KP = 2.5   #6.0 3.0
+PID_ROLL_KI = 1.0   #3.0 3.0
+PID_ROLL_KD = 3.5   #0.0 0.035
+PID_ROLL_INTEGRATION_LIMIT = 0.0   #20.0
 
-PID_PITCH_KP = 3.0  #6.0
-PID_PITCH_KI = 3.0  #3.0
-PID_PITCH_KD = 0.035  #0.0
-PID_PITCH_INTEGRATION_LIMIT = 20.0  #20.0
+PID_PITCH_KP = 0.25  #6.0 3.0
+PID_PITCH_KI = 1.0  #3.0 3.0
+PID_PITCH_KD = 3.5  #0.0 0.035
+PID_PITCH_INTEGRATION_LIMIT = 0.0  #20.0
 
-PID_YAW_KP = 6.0    #6.0
+PID_YAW_KP = 0.25    #6.0
 PID_YAW_KI = 1.0    #1.0
-PID_YAW_KD = 0.035   #0.35
-PID_YAW_INTEGRATION_LIMIT = 360.0   #360
+PID_YAW_KD = 3.5   #0.35
+PID_YAW_INTEGRATION_LIMIT = 0.0   #360
 
 
-DEFAULT_PID_INTEGRATION_LIMIT = 5000.0
-DEFAULT_PID_OUTPUT_LIMIT = 0.0
+DEFAULT_PID_INTEGRATION_LIMIT = 0.0 #5000.0
+DEFAULT_PID_OUTPUT_LIMIT = 0.0  #0.0
 # ----------------------------------------------------------------------------------------------------------------------
 #                                           P O S I T I O N  P I D  V A L U E S
 # ----------------------------------------------------------------------------------------------------------------------
+PID_VELOCITY_X_KP = 0.02    #25.0
+PID_VELOCITY_X_KI = 0.01     #1.0
+PID_VELOCITY_X_KD = 0.01     #0.0
 
+PID_VELOCITY_Y_KP = 0.02    #25.0
+PID_VELOCITY_Y_KI = 0.01     #1.0
+PID_VELOCITY_Y_KD = 0.01     #0.0
+
+PID_VELOCITY_Z_KP = 4.0   #25.0
+PID_VELOCITY_Z_KI = 0.0     #15.0
+PID_VELOCITY_Z_KD = 2.5   #0.0
+
+PID_POSITION_X_KP = 0.02    #2.0
+PID_POSITION_X_KI = 0.0     #0.0
+PID_POSITION_X_KD = 0.01     #0.0
+
+PID_POSITION_Y_KP = 0.02    #2.0
+PID_POSITION_Y_KI = 0.0     #0.0
+PID_POSITION_Y_KD = 0.01    #0.0
+
+PID_POSITION_Z_KP = 15.0   #2.0 10/15
+PID_POSITION_Z_KI = 0.1     #0.5 0.1
+PID_POSITION_Z_KD = 0.02     #0.0 0.2
 # ----------------------------------------------------------------------------------------------------------------------
 #                                           V E L O C I T Y  P I D  V A L U E S
 # ----------------------------------------------------------------------------------------------------------------------
