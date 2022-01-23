bfb5fc955596c6ab560f8524207dfbc0240a3057
Andrea Fuso <andrea1.fuso@gmail.com>
2022-01-23 21:00:57 +0100

    Added parameters section in launch file generator GUI

--- crazy_common_py/src/crazyflie_manager/CrazyAppManager.py
+++ crazy_common_py/src/crazyflie_manager/CrazyAppManager.py
@@ -1,18 +1,23 @@
 import sys
 from PyQt5.QtWidgets import *
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtGui import QPixmap, QMouseEvent, QCursor, QPainter, QPen
 from PyQt5 import QtGui, QtCore
-from PyQt5.QtGui import QCursor
+from PyQt5.QtCore import QSize
+from qtwidgets import Toggle
 
 from app_styles import *
-from custom_widgets import MainMenuButton, MenuButton
+from custom_widgets import MainMenuButton, MyMenuBar, GenericSection, MyButton
 
 def button_pressed():
     print('Button pressed!!')
 
-def show2():
+def show_launch_file_generator():
     launchfile_window.show()
     menu_window.hide()
+
+def menu_button_pressed():
+    menu_window.show()
+
 
 app = QApplication(sys.argv)
 
@@ -44,10 +49,10 @@ menu_logo.setStyleSheet('margin-top: 10px;')
 # ----------------------------------------------------------------------------------------------------------------------
 #                                                    B U T T O N S
 # ----------------------------------------------------------------------------------------------------------------------
-launch_file_gen_button = MainMenuButton('Launch file generator', '50px', button_pressed)
-export_urdf_button = MainMenuButton('Export URDF', '5px', button_pressed)
-task_launcher_button = MainMenuButton('Task launcher', '5px', button_pressed)
-settings_button = MainMenuButton('Settings', '5px', show2)
+launch_file_gen_button = MyButton('Launch file generator', '50px', show_launch_file_generator)
+export_urdf_button = MyButton('Export URDF', '5px', button_pressed)
+task_launcher_button = MyButton('Task launcher', '5px', button_pressed)
+settings_button = MyButton('Settings', '5px', button_pressed)
 
 # ----------------------------------------------------------------------------------------------------------------------
 #                                               A D D I N G  W I D J E T S
@@ -66,37 +71,210 @@ menu_window.setLayout(menu_grid)
 #                                               L A U N C H  F I L E  W I N D O W
 #
 # ======================================================================================================================
+# ----------------------------------------------------------------------------------------------------------------------
+#                                               F U N C T I O N S  & S E T T I N G S
+# ----------------------------------------------------------------------------------------------------------------------
+def sim_toggle_cb():
+    if lf_real_toggle.checkState() == 0:
+        lf_real_toggle.setChecked(True)
+    else:
+        lf_real_toggle.setChecked(False)
+def real_toggle_cb():
+    if lf_sim_toggle.checkState() == 0:
+        lf_sim_toggle.setChecked(True)
+    else:
+        lf_sim_toggle.setChecked(False)
+
+def cf_number_cb():
+    print('Modified')
+
 launchfile_window = QWidget()
 launchfile_window.setWindowTitle('Crazy App - Launch File Generator')
 launchfile_window.setFixedWidth(1000)
+launchfile_window.setFixedHeight(500)
 launchfile_window.setStyleSheet(f'background: {LIGHT_BACKGROUND_COLOR};')
 
-launchfile_outer_layout = QVBoxLayout()
-launchfile_menu_layout = QHBoxLayout()
-launchfile_menu_layout.setSpacing(0)
+# Master Layout:
+lf_master_layout = QVBoxLayout()
+
+# Titles layouts
+lf_titles = QHBoxLayout()
+# Main layout (parameters + preview):
 launchfile_main_layout = QHBoxLayout()
 
-menu_button = QLabel()
-menu_button_image_blu = QPixmap('AppImages/Menu_icon_blu.png').scaledToHeight(30)
-menu_button_image_white = QPixmap('AppImages/Menu_icon_white.png').scaledToHeight(30)
-menu_button.setPixmap(menu_button_image_blu)
-menu_button.setAlignment(QtCore.Qt.AlignLeft)
-menu_button.setStyleSheet(f'background: {MENU_COLOR}; padding: 10px;')
-launchfile_menu_layout.addWidget(menu_button)
+# ----------------------------------------------------------------------------------------------------------------------
+#                                               M E N U B A R
+# ----------------------------------------------------------------------------------------------------------------------
+launchfile_menubar = MyMenuBar(default_image_path='AppImages/Menu_icon_blu.png',
+                               hover_image_path='AppImages/Menu_icon_white.png',
+                               callback_function=menu_button_pressed)
+lf_master_layout.addLayout(launchfile_menubar.menubar)
+# ----------------------------------------------------------------------------------------------------------------------
+#                                               T I T L E S
+# ----------------------------------------------------------------------------------------------------------------------
+# PARAMETERS
+lf_parameters_title = GenericSection(200, 0, 'PARAMETERS').section_layout
+lf_titles.addLayout(lf_parameters_title)
+lf_titles.setAlignment(QtCore.Qt.AlignTop)
 
-launchfile_logo = QLabel()
-menu_logo_image = QPixmap('AppImages/Logo_white.png').scaledToHeight(30)
-launchfile_logo.setPixmap(menu_logo_image)
-launchfile_logo.setAlignment(QtCore.Qt.AlignRight)
-launchfile_logo.setStyleSheet(f'background: {MENU_COLOR}; padding: 10px;')
-launchfile_menu_layout.addWidget(launchfile_logo)
+# VERTICAL LINE
+lf_titles_vertical_line = QLabel()
+lf_titles_vertical_line.setFixedWidth(5)
+lf_titles_vertical_line.setStyleSheet(
+    f"background: {LIGHT_BACKGROUND_COLOR};"
+    "border-radius: 2px;"
+)
+lf_titles.addWidget(lf_titles_vertical_line)
+
+# PREVIEW:
+lf_preview_title = GenericSection(225, 0, 'PREVIEW').section_layout
+lf_titles.addLayout(lf_preview_title)
 
 
-launchfile_outer_layout.addLayout(launchfile_menu_layout)
-launchfile_outer_layout.addLayout(launchfile_main_layout)
+lf_master_layout.addLayout(lf_titles)
+# ----------------------------------------------------------------------------------------------------------------------
+#                                               P A R A M E T E R S  S E C T I O N
+# ----------------------------------------------------------------------------------------------------------------------
+lf_parameters = QVBoxLayout()
 
-launchfile_window.setLayout(launchfile_outer_layout)
+# NAME PARAMETER
+lf_name_label = QLabel('Name:')
+lf_name_label.setStyleSheet(
+    "font-weight: bold;"
+)
+lf_name_input_label = QLineEdit()
+lf_name_input_label.setText('my_launch')
 
+lf_name_param = QHBoxLayout()
+lf_name_param.addWidget(lf_name_label)
+lf_name_param.addWidget(lf_name_input_label)
 
-launchfile_window.show()
+# REAL OR SIMULATED
+lf_sim_param_label = QLabel('Simulated')
+lf_sim_param_label.setStyleSheet(
+    "font-weight: bold;"
+)
+lf_sim_toggle = Toggle()
+lf_sim_toggle.setFixedWidth(75)
+lf_sim_toggle.setChecked(True)
+lf_sim_toggle.clicked.connect(sim_toggle_cb)
+
+lf_real_param_label = QLabel('Real')
+lf_real_param_label.setStyleSheet(
+    "font-weight: bold;"
+)
+lf_real_toggle = Toggle()
+lf_real_toggle.setFixedWidth(75)
+lf_real_toggle.setChecked(False)
+lf_real_toggle.clicked.connect(real_toggle_cb)
+
+lf_real_sim_h_spacer = QSpacerItem(1000, 20, QSizePolicy.Maximum, QSizePolicy.Expanding)
+
+lf_real_sim_param = QHBoxLayout()
+lf_real_sim_param.addWidget(lf_sim_param_label)
+lf_real_sim_param.addWidget(lf_sim_toggle)
+lf_real_sim_param.addWidget(lf_real_param_label)
+lf_real_sim_param.addWidget(lf_real_toggle)
+lf_real_sim_param.addItem(lf_real_sim_h_spacer)
+
+# NUMBER OF CRAZYFLIES:
+lf_cf_number_label = QLabel('Number of crazyflies:')
+lf_cf_number_label.setStyleSheet(
+    "font-weight: bold;"
+)
+lf_cf_number_spinner = QSpinBox()
+lf_cf_number_spinner.setValue(1)
+lf_cf_number_spinner.valueChanged.connect(cf_number_cb)
+lf_cf_number_spinner.setFixedWidth(75)
+
+lf_cf_number_h_spacer = QSpacerItem(1000, 20, QSizePolicy.Maximum, QSizePolicy.Expanding)
+
+lf_cf_number_param = QHBoxLayout()
+lf_cf_number_param.addWidget(lf_cf_number_label)
+lf_cf_number_param.addWidget(lf_cf_number_spinner)
+lf_cf_number_param.addItem(lf_cf_number_h_spacer)
+
+# ADDITIONAL NODES:
+lf_nodes_label = QLabel('Add node:')
+lf_nodes_label.setStyleSheet(
+    "font-weight: bold;"
+)
+lf_nodes_input_label = QLineEdit()
+lf_nodes_input_label.setText('PackageName:NodeName:ScriptName')
+lf_nodes_input_label.setFixedWidth(300)
+lf_nodes_add_btn = MyButton('Add', '5px', menu_button_pressed).Button
+lf_nodes_add_btn.setFixedWidth(50)
+
+lf_nodes_h_spacer = QSpacerItem(1000, 20, QSizePolicy.Maximum, QSizePolicy.Expanding)
+
+lf_nodes_param = QHBoxLayout()
+lf_nodes_param.addWidget(lf_nodes_label)
+lf_nodes_param.addWidget(lf_nodes_input_label)
+lf_nodes_param.addWidget(lf_nodes_add_btn)
+#lf_nodes_param.addItem(lf_nodes_h_spacer)
+
+# ADDITIONAL LAUNCH FILES
+lf_add_lfs_label = QLabel('Add launch file:')
+lf_add_lfs_label.setStyleSheet(
+    "font-weight: bold;"
+)
+lf_add_lfs_input_label = QLineEdit()
+lf_add_lfs_input_label.setText('PackageName:LaunchFileName')
+lf_add_lfs_input_label.setFixedWidth(300)
+lf_add_lfs_add_btn = MyButton('Add', '5px', menu_button_pressed).Button
+lf_add_lfs_add_btn.setFixedWidth(50)
+
+lf_add_lfs_h_spacer = QSpacerItem(1000, 20, QSizePolicy.Maximum, QSizePolicy.Expanding)
+
+lf_add_lfs_param = QHBoxLayout()
+lf_add_lfs_param.addWidget(lf_add_lfs_label)
+lf_add_lfs_param.addWidget(lf_add_lfs_input_label)
+lf_add_lfs_param.addWidget(lf_add_lfs_add_btn)
+#lf_add_lfs_param.addItem(lf_add_lfs_h_spacer)
+
+# VERTICAL SPACER:
+lf_parameters_v_spacer = QSpacerItem(1000, 1000, QSizePolicy.Maximum, QSizePolicy.Expanding)
+
+# Adding parameters section:
+lf_parameters.addLayout(lf_name_param)
+lf_parameters.addLayout(lf_real_sim_param)
+lf_parameters.addLayout(lf_cf_number_param)
+lf_parameters.addLayout(lf_nodes_param)
+lf_parameters.addLayout(lf_add_lfs_param)
+
+lf_parameters.addItem(lf_parameters_v_spacer)
+launchfile_main_layout.addLayout(lf_parameters)
+
+# VERTICAL LINE
+lf_parameters_vertical_line = QLabel()
+lf_parameters_vertical_line.setFixedWidth(5)
+lf_parameters_vertical_line.setStyleSheet(
+    f"background: #D9D9D9;"
+    "border-radius: 2px;"
+)
+launchfile_main_layout.addWidget(lf_parameters_vertical_line)
+# ----------------------------------------------------------------------------------------------------------------------
+#                                               P R E V I E W  S E C T I O N
+# ----------------------------------------------------------------------------------------------------------------------
+lf_preview_group = QGroupBox()
+lf_preview_group.setFixedWidth(480)
+lf_preview_form = QFormLayout()
+
+lf_elements_list = []
+lf_elements_remove_btns = []
+lf_elements_positions = []
+
+launchfile_main_layout.addWidget(lf_preview_group)
+lf_master_layout.addLayout(launchfile_main_layout)
+# ----------------------------------------------------------------------------------------------------------------------
+#                                               B U T T O N S  S E C T I O N
+# ----------------------------------------------------------------------------------------------------------------------
+
+# SPACER
+vertical_spacer = QSpacerItem(1000, 20, QSizePolicy.Maximum, QSizePolicy.Expanding)
+
+lf_master_layout.addItem(vertical_spacer)
+launchfile_window.setLayout(lf_master_layout)
+
+menu_window.show()
 sys.exit(app.exec())
--- crazy_common_py/src/crazyflie_manager/custom_widgets.py
+++ crazy_common_py/src/crazyflie_manager/custom_widgets.py
@@ -1,7 +1,7 @@
 from app_styles import *
 from PyQt5.QtWidgets import *
 from PyQt5 import QtCore
-from PyQt5.QtGui import QCursor, QIcon, QPixmap
+from PyQt5.QtGui import QCursor, QIcon, QPixmap, QPainter
 # ======================================================================================================================
 #
 #                                               B U T T O N S
@@ -35,7 +35,10 @@ class MainMenuButton():
         )
         self.Button.clicked.connect(callback_function)
 
-class MenuButton():
+# ----------------------------------------------------------------------------------------------------------------------
+#                                              G E N E R I C  B U T T O N
+# ----------------------------------------------------------------------------------------------------------------------
+class MyButton():
     def __init__(self, text, margin_top, callback_function,
                  border=MENU_BTN_BORDER, border_radius=MENU_BTN_BORDER_RADIUS,
                  font_size=MENU_BTN_FONT_SIZE, font_family=MENU_BTN_FONT_FAMILY, font_weight=MENU_BTN_FONT_WEIGHT,
@@ -59,13 +62,72 @@ class MenuButton():
             f"background: '{background_hover_color}'; color: '{font_hover_color}';" + "}"
         )
         self.Button.clicked.connect(callback_function)
-        self.Button.setIcon(QIcon(QPixmap('AppImages/Menu_icon_white.png').scaledToHeight(30)))
-
 # ======================================================================================================================
 #
 #                                                   M E N U  B A R
 #
 # ======================================================================================================================
-class MenuBar():
-    def __init__(self):
-        pass
+class MyMenuBar():
+    def __init__(self, default_image_path, hover_image_path, callback_function):
+        launchfile_menu_layout = QHBoxLayout()
+        # Set spacing to zero to avoid visual space columnt:
+        launchfile_menu_layout.setSpacing(0)
+
+        menu_button_image_blu = QPixmap(default_image_path).scaledToHeight(30)
+        menu_button_image_white = QPixmap(hover_image_path).scaledToHeight(30)
+        menu_button_background = QLabel()
+        menu_button_background.setStyleSheet(f"background: {MENU_COLOR};")
+        menu_button_background.setFixedHeight(50)
+
+        menu_button = QPushButton(menu_button_background)
+        menu_button.setFixedSize(menu_button_image_blu.size())
+        menu_button.setStyleSheet(
+            "*{" +
+            f"background-image: url({default_image_path});" +
+            "border: 0px;" +
+            "}" +
+            "*:hover{" +
+            f"background-image: url({hover_image_path});" +
+            "}"
+        )
+        menu_button.move(menu_button.parent().pos().x() + 10, menu_button.parent().pos().y() +
+                         int(menu_button.parent().frameGeometry().height() / 2 - menu_button.frameGeometry().height() / 2))
+        menu_button.clicked.connect(callback_function)
+        launchfile_menu_layout.addWidget(menu_button_background)
+
+        launchfile_logo = QLabel()
+        menu_logo_image = QPixmap('AppImages/Logo_white.png').scaledToHeight(30)
+        launchfile_logo.setPixmap(menu_logo_image)
+        launchfile_logo.setAlignment(QtCore.Qt.AlignRight)
+        launchfile_logo.setStyleSheet(f'background: {MENU_COLOR}; padding: 10px;')
+        launchfile_logo.setFixedHeight(50)
+        launchfile_menu_layout.addWidget(launchfile_logo)
+        launchfile_menu_layout.setAlignment(QtCore.Qt.AlignTop)
+        self.menubar = launchfile_menu_layout
+
+# ======================================================================================================================
+#
+#                                                   S E C T I O N S
+#
+# ======================================================================================================================
+# ----------------------------------------------------------------------------------------------------------------------
+#                                             G E N E R I C  S E C T I O N
+# ----------------------------------------------------------------------------------------------------------------------
+class GenericSection():
+    def __init__(self, offsetx, offsety, text, background_color=MENU_COLOR, text_color='white'):
+        # PARAMETERS SECTION:
+        self.section_layout = QVBoxLayout()
+
+        # Title:
+        section_background = QLabel()
+        section_background.setStyleSheet(
+            f"background: {background_color};"
+        )
+        self.section_layout.addWidget(section_background)
+        title = QLabel(text, parent=section_background)
+        title.setStyleSheet(
+            f"color: {text_color};"
+        )
+        title.move(section_background.pos().x() + offsetx,
+                   section_background.pos().y() + offsety
+                   )
